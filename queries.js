09600c26160aab47b68e4f3c23ab1ba01112471d
mytzenka <39919027+mytzenka@users.noreply.github.com>
2023-02-11 13:50:15 +0100

    Heroku updates

--- queries.js
+++ queries.js
@@ -1,14 +1,23 @@
 require("express");
 require('dotenv').config();
 
-const Pool = require('pg').Pool
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     port: process.env.PG_PORT
+// })
+
+const { Pool } = require('pg');
 const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 
 const getWords = (request, response) => {
     pool.query('SELECT * FROM words ORDER BY word ASC', (error, results) => {
