3a08f87f0ad07fb405bb167903fc011736b2c483
Michiel <39919027+mytzenka@users.noreply.github.com>
2023-06-30 15:22:56 +0200

    Fix

--- backend.js
+++ backend.js
@@ -24,6 +24,19 @@ app.use(cors(corsOptions));
 app.get('/words', query.getWords)
 app.post('/word', query.addWord)
 app.get('/names', query.getNames)
+app.get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = { 'results': (result) ? result.rows : null};
+            res.render('pages/db', results );
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
+
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
