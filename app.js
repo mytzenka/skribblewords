f1d0d39e4561e188348016e1a3b69f9630fddfea
mytzenka <39919027+mytzenka@users.noreply.github.com>
2023-07-08 19:25:27 +0200

    Remove create tables

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
     query.testDB()
-    query.createTables()
+    // query.createTables()
 })
